746faf41a1474baa6e8e0570b490fdb8796afd0c
chutima <chutimamaa2544@gmail.com>
2022-08-29 21:02:15 +0700

    init

---
+++ lib/formula/math_formula.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MathFormula extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("การสั่งซื้อ"),
+        ),
+        body: Container()
+        );
+  }
+}
